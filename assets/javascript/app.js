eedee75e791f9119418b12e4293ac2f1c3b3b9e7
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-07-23 15:15:24 -0400

    updated question array in js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,56 @@
 //set all vars up here//
 var questions = [{
-    q: "q1",
-    answer: ["A1", "A2", "A3", "A4"],
-    correct: "A2",
+    q: "What is the sigil for House Mormont?",
+    answer: ["Lion", "Stag", "Bear", "Direwolf"],
+    correct: "Bear",
 },
 {
-    q: "q2",
-    answer: ["A1", "A2", "A3", "A4"],
-    correct: "A3",
+    q: "Who was responsible for creating the Night King?",
+    answer: ["The Lord of Light", "The Children of the Forest", "The Drowned God", "The First Men"],
+    correct: "The Children of the Forest",
+},
+{
+    q: "Who was the leader of the Golden Company sellsword when Dany ransacked King's Landing? ",
+    answer: ["Wes Borland", "Harry Strickland", "Kiefer Sutherland", "Robert Westland"],
+    correct: "Harry Strickland",
+},
+{
+    q: "Who is Jon Snow's real father?",
+    answer: ["Ned Stark", "Rhaegar Targaryen", "Aegon Targaryen", "Robert Baratheon"],
+    correct: "Rhaegar Targaryen",
+},
+{
+    q: "How many seasons did Game of Thrones have as a TV show?",
+    answer: ["8", "7", "9", "6"],
+    correct: "8",
+},
+{
+    q: "What was the Valyrian Steel sword's name that Jon Snow received from Lord Commander Mormont?",
+    answer: ["Bearclaw", "Needle", "Ice", "Longclaw"],
+    correct: "Longclaw",
+},
+{
+    q: "How many children did Cersei have?",
+    answer: ["1", "2", "3", "4"],
+    correct: "3",
+},
+{
+    q: "What House did Jojen and Meera belong to?",
+    answer: ["Tully", "Reed", "Redwyne", "Arryn"],
+    correct: "Reed",
+},
+{
+    q: "Who brought Jon Snow back to life?",
+    answer: ["Daenerys", "Drogon", "Melisandre", "Night King"],
+    correct: "Melisandre",
+},
+{
+    q: "What did Stannis Baratheon call Davos Seaworth?",
+    answer: ["Potato Knight", "Bread Knight", "Wine Knight", "Onion Knight"],
+    correct: "Onion Knight",
 }];
 
-var count = 5;
+var count = 30;
 var elem = $('#seconds');
 var timerId;
 var usedQ = [];
@@ -20,12 +60,14 @@ var questionNum = 0; // this serves as index//
 //functions//
 
 
+
+
 function countdown() {
     if (count == 0) {
         clearTimeout(timerId);
         questionNum++;
         setQuestion();
-        
+
         console.log(timerId)
 
     } else {
@@ -38,26 +80,36 @@ function countdown() {
 }
 
 function setQuestion() {
-    
-    count = 5;
-    timerId=setInterval(countdown, 1000); 
+
+    count = 30;
+    timerId = setInterval(countdown, 1000);
     $('#questions').html(questions[questionNum].q);
 
     //need for loop for answers// --> 
-    for(var i = 0; i < questions[questionNum].answer.length; i++) {
-        var button =  $("<button>")
-      
-       button.text(questions[questionNum].answer[i]);
+    for (var i = 0; i < questions[questionNum].answer.length; i++) {
+        var button = $('<button>' + questions[questionNum].answer[i] + '</button')
 
+        button.appendTo('#answerDiv')
+
+
+        // button.html(questions[questionNum].answer[i]);
+        // button.attr(questions[questionNum].answer[i])
     }
-   
-    
-    // var questionStore = Math.floor((Math.random() * questions.length)); randomly generates questions from array
 
+    if ($(this).text()===questions[questionNum.correct])
+    win++;
+
+    else {
+        lose++;
+    }
+    
+    //need to have an on click event for the buttons and compare that with the "correct" answer in the questions array//
+    
     //setting question will be in questions array//
-    console.log(questions[0].q);
 }
 
-// $("#questionDiv").on("click",);
-
+// $("#answerDiv").on("click",);
 setQuestion();
+
+
+// var questionStore = Math.floor((Math.random() * questions.length)); randomly generates questions from array
